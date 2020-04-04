30fa410521b1b524b9590d4da7de960344cbf1b0
PeterHolzer75 <peter@holzer.se>
2020-04-04 12:34:32 +0200

    lördag

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Address(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return (self.name)
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=250)
+    text = models.TextField()
+    adress = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    ShowtimeFrom = models.DateTimeField()
+    Showtimeto = models.DateTimeField()
+    ShowtimeSeconds = models.IntegerField(default=0)
+
+    def __str__(self):
+        return (self.name)
